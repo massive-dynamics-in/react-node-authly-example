2abd97e85b004d01084196291e8d5191c847e1ed
manjujidagi <manjujidagi@protonmail.com>
2025-03-15 20:16:01 +0530

    Improved login page

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Grid2 from '@mui/material/Grid2';
+import { Button, Stack, Typography } from '@mui/material';
 
 class Login extends React.Component {
 
@@ -18,12 +20,29 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <button
-                    onClick={this.loginWithAuthly}
-                >Login with Authly.ME</button>
-            </div>
+
+
+            <Grid2 container justifyContent={'center'} alignItems={'center'} sx={{height: '100vh'}}>
+                <Stack spacing={2} alignItems={'center'}>
+                    <Typography variant={'h4'}>Welcome</Typography>
+                    <Typography variant={'h6'} sx={{ color: 'gray' }}>Please login to continue</Typography>
+                    <Button
+                        size='small'
+                        variant={'outlined'}
+                        onClick={this.loginWithAuthly}
+                        startIcon={<img src="https://authly.me/images/logo/logo.png" alt="Authly Icon" style={{ width: '30px', height: '30px' }} />}
+                        sx={{
+                            borderColor: '#FF5722',
+                            color: '#FF5722',
+                            fontWeight: 'bold'
+                        }}
+                    >
+                        Login with Authly.ME
+                    </Button>
+                </Stack>
+                
+            </Grid2>
+
         );
     }
 }
